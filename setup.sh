dc1b771e621269bfbcb84e05bf0c5fec4c95fd20
develop <develop@kaaiot.io>
2018-11-13 23:10:43 +0200

    Packer for build Ubuntu 16.04 with custom docker(from source).

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+wget https://download.docker.com/linux/static/stable/x86_64/docker-17.12.1-ce.tgz
+tar xzvf docker-17.12.1-ce.tgz
+sudo cp docker/* /usr/bin/
+sudo mv ~/docker.service /lib/systemd/system/docker.service
+sudo systemctl start docker
+sudo systemctl enable docker
